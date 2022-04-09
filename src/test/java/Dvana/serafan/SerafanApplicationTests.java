e51da544531e59fe6b832de8150ebb64551aa3d0
Dvanna <hisokadara@gmail.com>
2022-04-09 09:54:50 +0300

    Initial commit 3:30 Минута написания. Тест.

---
+++ src/test/java/Dvana/serafan/SerafanApplicationTests.java
@@ -0,0 +1,13 @@
+package Dvana.serafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
